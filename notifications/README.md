07830f12502dccb4b7bfbc9f65ea2590862f9a4e
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-07-14 22:31:04 -0300

    add notifications module

---
+++ notifications/README.md
@@ -0,0 +1,3 @@
+# Notifications Library for ARGOS-HIDS
+
+This folder contains the implementation for the notifications library used by ARGOS to send out notifications when potential intrusions are detected. It exposes utility functions that can be used to send out the notifications.
---
+++ notifications/ntfy.py
@@ -0,0 +1,30 @@
+"""Module for sending intrusion detection notifications."""
+
+import requests
+from typing import List
+
+def notify_push(topic: str, message: str, title: str, tags: List[str], priority: int = 3) -> requests.Response:
+    """
+    Sends a push notification to Ntfy's default web server to be forwarded to subscribed devices.
+
+    Args:
+        topic (str): The ntfy topic to send the notification to.
+        message (str): The message to send in the notification.
+        title (str): The title of the notification.
+        tags (List[str]): A list of tags to associate with the notification.
+        priority (int, optional): The priority level of the notification (default is 3).
+    
+    Returns:
+        requests.Response: The response object from the ntfy server.
+    """
+    url = f"https://ntfy.sh/{topic}"
+
+    headers = {}
+    if title:
+        headers["Title"] = title
+    if tags:
+        headers["Tags"] = ",".join(tags)
+    if priority:
+        headers["Priority"] = str(priority)
+    
+    return requests.post(url, data=message, headers=headers)
