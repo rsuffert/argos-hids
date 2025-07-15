e397c0bc9a204d740da0e1c8ccc796db91dcdc7e
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-07-15 00:59:26 -0300

    add timeout param to ntfy notify cunv

--- notifications/ntfy.py
+++ notifications/ntfy.py
@@ -3,7 +3,9 @@
 import requests
 from typing import List
 
-def notify_push(topic: str, message: str, title: str, tags: List[str], priority: int = 3) -> requests.Response:
+def notify_push(
+    topic: str, message: str, title: str, tags: List[str], priority: int = 3, timeout: int = 5
+) -> requests.Response:
     """
     Sends a push notification to Ntfy's default web server to be forwarded to subscribed devices.
 
@@ -13,7 +15,8 @@ def notify_push(topic: str, message: str, title: str, tags: List[str], priority:
         title (str): The title of the notification.
         tags (List[str]): A list of tags to associate with the notification.
         priority (int, optional): The priority level of the notification (default is 3).
-    
+        timeout (int, optional): The timeout for the request in seconds (default is 5).
+
     Returns:
         requests.Response: The response object from the ntfy server.
     """
@@ -27,4 +30,4 @@ def notify_push(topic: str, message: str, title: str, tags: List[str], priority:
     if priority:
         headers["Priority"] = str(priority)
     
-    return requests.post(url, data=message, headers=headers)
+    return requests.post(url, data=message, headers=headers, timeout=timeout)
