42d1d88165acafcf26650f8a2ad8372f80b671b7
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-08-23 22:35:34 -0300

    integrate inference with trained model into main pipeline

--- main.py
+++ main.py
@@ -3,20 +3,32 @@
 import os
 import sys
 import time
+import torch
 import socket
 import signal
 import logging
+from typing import Dict, List
+from collections import defaultdict
 from argparse import ArgumentParser
 from notifications.ntfy import notify_push, Priority
 from tetragon.monitor import TetragonMonitor
+from models.lstm.trainer import LSTMClassifier, MAX_SEQ_LEN
 
 ARGOS_NTFY_TOPIC = os.getenv("ARGOS_NTFY_TOPIC")
 MACHINE_NAME = os.getenv("MACHINE_NAME", socket.gethostname())
+TRAINED_MODEL_PATH = os.getenv("TRAINED_MODEL_PATH",
+    os.path.join(
+        os.path.dirname(__file__), "models", "lstm", "lightning_logs", "version_0", "checkpoints", "best-val-f1.ckpt"
+    )
+)
 
 def main() -> None:
     """Main function to run the ARGOS HIDS."""
     if not ARGOS_NTFY_TOPIC:
         logging.error("ARGOS_NTFY_TOPIC environment variable is not set.")
+        sys.exit(1)
+    if not os.path.exists(TRAINED_MODEL_PATH):
+        logging.error(f"Trained model file not found: {TRAINED_MODEL_PATH}")
         sys.exit(1)
     logging.info(f"Starting ARGOS HIDS on machine '{MACHINE_NAME}'")
 
@@ -29,9 +41,18 @@ def main() -> None:
     signal.signal(signal.SIGINT, handle_signal) # ctrl+c
     signal.signal(signal.SIGTERM, handle_signal) # kill
 
-    # start monitoring and classifying syscalls
+    # instantiate the intrusion detection model
+    model = LSTMClassifier.load_from_checkpoint(TRAINED_MODEL_PATH)
+    model.eval()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    # TODO: load syscall names to IDs mapping used during training
+    syscall_names_to_ids: Dict[str, int] = defaultdict(lambda: -1)
+
     with TetragonMonitor() as monitor:
+        pids_to_syscalls: Dict[int, List[int]] = defaultdict(list)
         while running:
+            # receive syscalls from Tetragon
             pid, syscall = monitor.get_next_syscall_name()
             if pid is None or syscall is None:
                 logging.info("No new syscalls to analyze. Sleeping for a few moments...")
@@ -39,8 +60,21 @@ def main() -> None:
                 continue
             logging.debug(f"Received - PID: {pid}, syscall_id: {syscall}")
 
-            # TODO: Classify syscall sequences received from Tetragon
-            malicious = False
+            # accumulate syscalls until ready to classify
+            pids_to_syscalls[pid].append(syscall_names_to_ids[syscall])
+            syscalls_from_current_pid = pids_to_syscalls[pid]
+            if len(syscalls_from_current_pid) < MAX_SEQ_LEN:
+                continue
+
+            # classify syscall sequences
+            sequences_tensor = torch.tensor(syscalls_from_current_pid, dtype=torch.long).unsqueeze(0).to(device)
+            lengths_tensor = torch.tensor([MAX_SEQ_LEN], dtype=torch.long).to(device)
+            with torch.no_grad():
+                outputs = model(sequences_tensor, lengths_tensor)
+            predicted_class = torch.argmax(outputs, dim=1).item()
+
+            # send intrusion detection notification for malicious sequences
+            malicious = (predicted_class == 1)
             if malicious:            
                 logging.warning(f"Malicious syscall sequence detected from PID {pid}.")
                 logging.info("Sending intrusion detection notification.")
